8d29b7a4b53d6a880820b3d903b6f85190a9d491
Qingzhou Yan <qingzhouyan@gmail.com>
2022-10-24 20:22:58 -1000

    persist current user info in localStorage by redux

--- src/index.js
+++ src/index.js
@@ -4,16 +4,19 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import store from './redux/store';
+import store, { persistor } from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
-        <App />
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
       </Provider>
     </BrowserRouter>
   </React.StrictMode>,
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,20 @@
 import { createStore, combineReducers } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import currUserReducer from './reducers/currUser';
 import dialogReducer from './reducers/dialog';
 
+const persistConfig = {
+  key: 'root',
+  storage,
+  blacklist: ['dialog'],
+};
+
 // combine reducers
 const allReducers = combineReducers({ currUser: currUserReducer, dialog: dialogReducer });
-
-export default createStore(allReducers, composeWithDevTools());
+const persistedReducer = persistReducer(persistConfig, allReducers);
+const store = createStore(persistedReducer, composeWithDevTools());
+const persistor = persistStore(store);
+export default store;
+export { persistor };
